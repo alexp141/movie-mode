ca71c8104f4505886a1be5f4a728ded2accadb3b
Alexander Pastor <apastor@umd.edu>
2024-02-09 13:58:11 -0500

    Added about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Section = styled.section`
@@ -5,12 +6,71 @@ const Section = styled.section`
 
   & h3 {
   }
+
+  & ul {
+    margin-left: 1.4rem;
+  }
+
+  & li {
+    list-style: inside;
+  }
 `;
+const StyledLink = styled(Link)`
+  &:link,
+  &:visited {
+    color: var(--color-purple);
+    font-size: 2.2rem;
+    font-weight: 500;
+    padding: 1.2rem 2.4rem;
+    transition: all 0.3s;
+  }
+
+  &:hover,
+  &:active {
+    color: var(--color-purple-light-30);
+  }
+`;
+
 export default function About() {
   return (
-    <Section>
-      <h3>General</h3>
-      <p>general info</p>
-    </Section>
+    <>
+      <Section>
+        <h2>General</h2>
+        <p>
+          This is a movie/TV watchlist website created by Alexander Pastor using
+          ReactJS.
+        </p>
+      </Section>
+      <Section>
+        <h2>Technologies Used</h2>
+        <ul>
+          <li>ReactJS</li>
+          <li>Tanstack React Query</li>
+          <li>React Hook Form</li>
+          <li>React Icons</li>
+          <li>React Toastify</li>
+          <li>Styled Components</li>
+          <li>Supabase (PostgreSQL)</li>
+          <li>React Router</li>
+          <li>Dotenv</li>
+        </ul>
+      </Section>
+      <Section>
+        <h2>APIs Used</h2>
+        <ul>
+          <li>
+            <StyledLink to="https://fanart.tv/">FanartTv</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="https://www.omdbapi.com/">OMDB API</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="https://simkl.docs.apiary.io/#">
+              Simkl API
+            </StyledLink>
+          </li>
+        </ul>
+      </Section>
+    </>
   );
 }
